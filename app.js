beb3f23e691b66e0db02d25f392d9524c3d50c05
adm-sabitovka <sabitovka@shkd.bizml.ru>
2022-09-28 15:20:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({extended: true}));
+
+const PORT = 5000;
+
+async function start() {
+  try {
+    app.listen(5000, () => console.log(`App has been started on ${PORT}...`));
+  } catch(e) {
+    console.log('Server error ', e.message);
+    process.exit(1);
+  }
+}
+
+start();

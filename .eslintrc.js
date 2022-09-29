ee79d815a03d46aea91b73c2520670c148fad40d
adm-sabitovka <sabitovka@shkd.bizml.ru>
2022-09-29 07:25:25 +0300

    Added eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // enable additional rules
+    'linebreak-style': ['error', 'unix'],
+    indent: ['error', 2],
+    semi: ['error', 'always'],
+    eqeqeq: [2, 'smart'],
+    // override configuration set by extending "eslint:recommended"
+    'no-empty': 'warn',
+    'no-cond-assign': ['error', 'always'],
+    'no-unused-expressions': [2, { allowShortCircuit: true }],
+    'no-restricted-globals': ['error', 'fdescribe'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'ignore',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+
+    // disable rules from base configurations
+    'for-direction': 'off',
+    'max-len': [
+      'error', 150, 2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ]
+  }
+};
--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json({extended: true}));
+app.use(express.json({ extended: true }));
 
 const PORT = 5000;
 
 async function start() {
   try {
     app.listen(5000, () => console.log(`App has been started on ${PORT}...`));
-  } catch(e) {
+  } catch (e) {
     console.log('Server error ', e.message);
     process.exit(1);
   }

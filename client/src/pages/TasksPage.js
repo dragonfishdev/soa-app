fe1255d9404e363fcd59e1d6a07f068d977bb1aa
adm-sabitovka <sabitovka@shkd.bizml.ru>
2022-10-06 15:24:27 +0300

    Added tasks page

---
+++ client/src/pages/TasksPage.js
@@ -0,0 +1,12 @@
+import { Task } from "../components/Task"
+
+export const TasksPage = () => {
+  return (
+    <ul class="collection">
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+    </ul>
+  )
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom"
-
 import { LoginPage } from './pages/LoginPage'
+import { TasksPage } from "./pages/TasksPage"
 
 export const useRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" exact element={<LoginPage />} />
+      <Route path="/login" exact element={ <LoginPage /> } />
+      <Route path="/" exact element={ <TasksPage /> } />
     </Routes>
   )
 }

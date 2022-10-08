217846a31598e8a2ffc10d7f38e87d0203b2f491
sabkar <karim13.sab@gmail.com>
2022-10-08 14:40:05 +0300

    Added authorization handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes";
+import { useAuth } from "./hooks/auth.hook"
+import { AuthContext } from "./context/AuthContext"
+import { Header } from "./components/Header";
+import { Loader } from "./components/Loader"
 
 import 'materialize-css';
-import { Header } from "./components/Header";
 
 function App() {
-  const routes = useRoutes();
+  const { token, login, logout, userId, ready } = useAuth()
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
+
+  if (!ready) {
+    return <Loader />
+  }
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        { routes }
-      </div>
-    </Router>
+    <AuthContext.Provider value={{
+      token, login, logout, userId, ready
+    }}>
+      <Router>
+        {isAuthenticated && <Header />}
+        <div className="container">
+          { routes }
+        </div>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 

5e412a04393fdacccd2dfbe91e740dd890780c3d
sabkar <karim13.sab@gmail.com>
2022-10-14 22:47:38 +0300

    Project setup

---
+++ auth-service/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+async function start() {
+  try {
+    app.listen(PORT, () => console.log(`App has been started on ${PORT}...`));
+  } catch (e) {
+    console.log('Server error ', e.message);
+    process.exit(1);
+  }
+}
+
+start();

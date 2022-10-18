ed49cc5df00d55b315b059bb0af45700676ab7d1
sabkar <karim13.sab@gmail.com>
2022-10-18 18:46:07 +0300

    User service migrated to mysql

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
 const models = require('./models');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(cors());
 
-app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/users', require('./routes/users.routes'));
 
 const PORT = 5000;
@@ -16,12 +15,6 @@ const PORT = 5000;
 async function start() {
   try {
     await models.sequelize.sync();
-
-    const admin = await models.User.findOne({ where: { userName: 'admin' } });
-    if (!admin) {
-      models.User.create({ userName: 'admin', email: 'sabitovka@shkd.bizml.ru', password: await bcrypt.hash('admin', 12), role: 'admin' });
-    }
-
     app.listen(5000, () => console.log(`App has been started on ${PORT}...`));
   } catch (e) {
     console.log('Server error ', e.message);

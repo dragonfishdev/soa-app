7636c86394c4eaf686ab9ba77f84ae520304dcbd
sabkar <karim13.sab@gmail.com>
2022-10-19 22:07:19 +0300

    Started registering handling process

--- auth-service/src/routes/auth.routes.js
+++ auth-service/src/routes/auth.routes.js
@@ -3,6 +3,7 @@ const { check, validationResult } = require('express-validator');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth.middleware');
 
 const router = Router();
 
@@ -12,41 +13,43 @@ router.post(
     check('userName', 'Имя пользователя не должно быть пустым').not().isEmpty(),
     check('email').isEmail(),
   ],
-  // async (req, res) => {
-  //   try {
-  //     const validationErrors = validationResult(req);
-  //     if (!validationErrors.isEmpty()) {
-  //       return res.status(400).json({
-  //         errors: validationErrors.array(),
-  //         message: 'Некорректные данные при регистрации',
-  //       });
-  //     }
+  auth,
+  async (req, res) => {
+    return res.sendStatus(200);
+    // try {
+    //   const validationErrors = validationResult(req);
+    //   if (!validationErrors.isEmpty()) {
+    //     return res.status(400).json({
+    //       errors: validationErrors.a rray(),
+    //       message: 'Некорректные данные при регистрации',
+    //     });
+    //   }
 
-  //     const { userName, email } = req.body;
+    //   const { userName, email } = req.body;
 
-  //     const candidate = await User.findOne({
-  //       where: {
-  //         [Op.or]: {
-  //           email,
-  //           userName,
-  //         },
-  //       },
-  //     });
-  //     if (candidate) {
-  //       return res.status(400).json({ message: 'Пользователь уже существует' });
-  //     };
+    //   const candidate = await User.findOne({
+    //     where: {
+    //       [Op.or]: {
+    //         email,
+    //         userName,
+    //       },
+    //     },
+    //   });
+    //   if (candidate) {
+    //     return res.status(400).json({ message: 'Пользователь уже существует' });
+    //   };
 
-  //     const password = 'test';
+    //   const password = 'test';
 
-  //     const hashedPassword = await bcrypt.hash(password, 12);
-  //     await User.create({ userName, email, password: hashedPassword });
+    //   const hashedPassword = await bcrypt.hash(password, 12);
+    //   await User.create({ userName, email, password: hashedPassword });
 
-  //     return res.status(201).json({ message: 'Пользователь создан' });
-  //   } catch (e) {
-  //     console.error(e);
-  //     return res.status(500).json({ message: 'Что-то пошло не так' });
-  //   }
-  // }
+    //   return res.status(201).json({ message: 'Пользователь создан' });
+    // } catch (e) {
+    //   console.error(e);
+    //   return res.status(500).json({ message: 'Что-то пошло не так' });
+    // }
+  },
 );
 
 router.post(

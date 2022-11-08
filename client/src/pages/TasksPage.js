f22116cf621fcc2a4ef3c2ad244e6873ad554eea
sabkar <karim13.sab@gmail.com>
2022-11-08 21:25:43 +0300

    Added changing status drag&dropping

--- client/src/pages/TasksPage.js
+++ client/src/pages/TasksPage.js
@@ -1,20 +1,71 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useHttp } from "../hooks/http.hook";
-import { TASKS_TYPE } from "../components/Tasks"
 import { Task } from "../components/Tasks/Task";
 import { Loader } from "../components/Loader";
 
+const TASKS_TYPE = {
+  NEW: {
+    name: 'NEW',
+    title: 'К выполнению',
+    postfix: 'new',
+    index: 0,
+  },
+  DOING: {
+    name: 'DOING',
+    title: 'Выполняется',
+    postfix: 'active',
+    index: 1,
+  },
+  COMPLETED: {
+    name: 'COMPLETED',
+    title: 'Завершено',
+    postfix: 'completed',
+    index: 2,
+  }
+}
+
 export const TasksPage = () => {
   const { loading, request } = useHttp();
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState({});
+  const dragItem = useRef();
+  const dragOverItem = useRef();
  
   const fetchTasks = useCallback(async () => {
     try {
       const fetched = await request('/api/tasks');
-      setTasks(fetched);
+      const tasksByType = {}
+      Object.values(TASKS_TYPE).forEach((val) => {
+        tasksByType[val.name] = {
+          ...val,
+          items: fetched.filter((task) => task.status === val.name),
+        }
+      });
+      setTasks(tasksByType);
     } catch (e) {}
   }, [request]);
-
+  
+  const dragStart = useCallback((e, type, position) => {
+    dragItem.current = { type, position }
+  }, []);
+  
+  const dragEnter = useCallback((e, type) => {
+    dragOverItem.current = type
+  }, []);
+  
+  const drop = useCallback(async (e) => {
+    if (dragItem.current.type === dragOverItem.current.name) {
+      return;
+    }
+    const task = tasks[dragItem.current.type].items[dragItem.current.position];
+    const newTask = await request(`/api/tasks/${task.id}/status`, 'PUT', { status: dragOverItem.current.index });
+    const copyTasks = { ...tasks };
+    copyTasks[task.status].items.splice(dragItem.current.position, 1)
+    copyTasks[dragOverItem.current.name].items.push(newTask)
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setTasks(copyTasks);
+  }, [tasks, request])
+  
   useEffect(() => {
     fetchTasks();
   }, [fetchTasks]);
@@ -22,38 +73,31 @@ export const TasksPage = () => {
   if (loading) {
     return <Loader />
   }
-
   
-  const tasksByType = {}
-  Object.values(TASKS_TYPE).forEach((val) => {
-    tasksByType[val.name] = {
-      ...val,
-      items: tasks.filter((task) => task.status === val.name),
-    }
-  });
-  
-  console.log(tasksByType);
   return (
     <div className="tasks-container">
       {
-        Object.values(tasksByType).map((type) => (
+        Object.values(tasks).map((type) => (
           <div
             key={type.name}
-            className={`task-card-stack task-card-stack_${type.postfix}`}>
-            <div className="task-card-stack__title">{type.title}</div>
-            <div className="task-card-stack__wrapper">
-              {
-                type.items.length
-                  ? type.items.map((task) => (
-                  <Task
-                    key={task.id}
-                    id={task.id} 
-                    title={task.title}
-                    description={task.description} />
-                ))
-                  : 'Пока нет задач'
-              }
-            </div>
+            className={`task-card-stack task-card-stack_${type.postfix}`}
+            onDragEnter={(e) => dragEnter(e, type)}
+            onDragEnd={drop}>
+              <div className="task-card-stack__title">{type.title}</div>
+              <div className="task-card-stack__wrapper">
+                {
+                  type.items.length
+                    ? type.items.map((task, index) => (
+                    <Task
+                      key={task.id}
+                      id={task.id} 
+                      title={task.title}
+                      description={task.description}
+                      onDragStart={(e) => dragStart(e, type.name, index)} />
+                  ))
+                    : 'Пока нет задач'
+                }
+              </div>
           </div>  
         ))
       }
